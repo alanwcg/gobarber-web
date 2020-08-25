7536a59f66ee1b187bc89421d2793b1969054f87
Alan Cintra <alancintra7@gmail.com>
2020-08-25 17:09:37 -0300

    Authentication and Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyles />
-    <BrowserRouter />
   </>
 );
 
